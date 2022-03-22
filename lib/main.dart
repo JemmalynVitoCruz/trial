94319c2cb9083e51e3b7d4466e65bea15127bd8b
lallaina <reginamaebulawan@gmail.com>
2022-03-22 16:38:53 -0400

    Updated device_upload

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './ui/login.dart';
 import 'ui/create_account.dart';
 import 'ui/select_method.dart';
+import 'ui/device_upload.dart';
 
 void main(){
   runApp(
@@ -10,8 +11,9 @@ void main(){
         title: "My First Flutter App",
         //home: LoginScreen(),
         //home: CreateAcc(),
-        home: SelectMethod(),
-        //home: DeviceUpload(),
+        //home: SelectMethod(),
+        home: DeviceUpload(),
+        //home: UploadFromLocal(),
       )
   );
 }
